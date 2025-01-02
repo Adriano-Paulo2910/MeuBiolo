a35f361eeff26aab45bc2504c317dc91cf6706d7
Adriano-Paulo2910 <paulocadeth100@gmail.com>
2025-01-02 14:55:27 +0100

    Criacao NavBar

--- app-front/src/components/Navbar.tsx
+++ app-front/src/components/Navbar.tsx
@@ -1,2 +1,18 @@
-import React from 'react';
 
+const Navbar = () => {
+    return (
+        <header className="flex p-4">
+            <div className="flex">
+                <h3 className="text-2xl font-semibold"><span>Meu</span>Biolo</h3>
+                <input placeholder="Location" className="border border-spacing-1 w-64 p-2 border-black ml-7" />
+                <input placeholder="Encontre os itens aqui..." className="border border-spacing-1 border-black w-90 p-2 ml-5" />
+            </div>
+
+            <div>
+                
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
